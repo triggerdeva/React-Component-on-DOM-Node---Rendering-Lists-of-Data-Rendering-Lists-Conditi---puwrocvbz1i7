73dcd88339f768326764b6c18318b59b9d33bdda
DEVENDRA MESHRAM <120394539+triggerdeva@users.noreply.github.com>
2023-03-27 19:02:35 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-
-
+const domNode = document.getElementById("root");
+ReactDOM.render(<App />, domNode);
